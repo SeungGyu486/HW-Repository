9142b97366eba663ceacb35648d1fcce6e2f2493
Lothric <Lothric@DESKTOP-NLVNHTP>
2022-06-13 11:33:23 +0900

    changed

--- MyGit/src/HW/A.java
+++ MyGit/src/HW/A.java
@@ -4,7 +4,9 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Hello. I'm A.java.");
-
+		System.out.println("devB branch_ changed. Ajava");
+		System.out.println("Master branch_ changed. Ajava");
+		System.out.println("devC branch_ changed. Ajava");
 	}
 
 }
--- MyGit/src/HW/C.java
+++ MyGit/src/HW/C.java
@@ -4,7 +4,7 @@ public class C {
 
 	public static void main(String[] args) {
 		System.out.println("Hello. I'm C.java.");
-
+		System.out.println("devC branch_ changed.Cjava");
 	}
 
 }
